2fe5e3638b9e3435ee157e00d7af12a37bfeb6a8
Paul Pflugradt <paulpflugradt@googlemail.com>
2016-04-09 18:48:28 +0200

    Updates

---
+++ dropdown.vue
@@ -0,0 +1,30 @@
+<template lang="jade">
+.container
+  button.btn Click
+    dropdown
+      li
+        a Content
+      li
+        a Content
+  a(href="https://github.com/paulpflug/vue-materialize/blob/master/dev/dropdown.vue") source
+</template>
+
+<script lang="coffee">
+require("./materialize.config.scss")
+module.exports =
+  components:
+    "dropdown" : require "../src/dropdown.coffee"
+
+</script>
+
+<style lang="stylus" scoped>
+.container > a
+  position absolute
+  left 40%
+  top 14px
+button
+  position absolute
+  &:nth-child(1)
+    top 10px
+    left 10px
+</style>
---
+++ modal.vue
@@ -0,0 +1,44 @@
+<template lang="jade">
+.container
+  button.btn click
+    modal
+      .modal-content
+        h4 Header
+        p Content
+      .modal-footer
+        closer.btn.btn-flat close
+  button.btn bottom-sheet
+    modal(bottom-sheet)
+      .modal-content
+        p Content
+      .modal-footer
+        closer.btn.btn-flat close
+  a(href="https://github.com/paulpflug/vue-materialize/blob/master/dev/modal.vue") source
+</template>
+
+<script lang="coffee">
+require("./materialize.config.scss")
+module.exports =
+  components:
+    "modal" : require "../src/modal.coffee"
+    "closer":
+      template: "<a @click.prevent='close'>close</a>"
+      methods:
+        close: ->
+          @$dispatch "close"
+</script>
+
+<style lang="stylus" scoped>
+.container > a
+  position absolute
+  left 250px
+  top 40px
+button
+  position absolute
+  &:nth-child(1)
+    top 10px
+    left 10px
+  &:nth-child(2)
+    top 80px
+    left 10px
+</style>
