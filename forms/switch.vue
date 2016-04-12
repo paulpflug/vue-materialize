6415fd0be00fcf8a68d96dc0078a509e122a60de
Paul Pflugradt <paulpflugradt@googlemail.com>
2016-04-12 13:47:41 +0200

    Updates

---
+++ forms/switch.vue
@@ -0,0 +1,25 @@
+<template lang="jade">
+.container
+  p
+    a(href="https://github.com/paulpflug/vue-materialize/blob/master/dev/forms/switch.vue") source
+  p
+    switch(@on="onOn" @off="onOff" @toggle="onToggle")
+  p
+    switch(disabled)
+  p Open console for events
+
+</template>
+
+<script lang="coffee">
+require("../materialize.config.scss")
+module.exports =
+  components:
+    "switch" : require "../../src/switch.vue"
+  methods:
+    onOn: ->
+      console.log "isOn"
+    onOff: ->
+      console.log "isOff"
+    onToggle: ->
+      console.log "isToggled"
+</script>
