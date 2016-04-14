cb92bbc40c40800ff55cf4846bd9bb4f1f3397a6
Paul Pflugradt <paulpflugradt@googlemail.com>
2016-04-14 15:33:49 +0200

    Updates

---
+++ tooltip.vue
@@ -0,0 +1,22 @@
+<template lang="jade">
+.container
+  p
+    a(href="https://github.com/paulpflug/vue-materialize/blob/master/dev/tooltip.vue") source
+  button.btn(style="margin-right: 10px") Hover
+    tooltip Content
+  button.btn(style="margin-right: 10px") Hover
+    tooltip(anchor="n") Content
+  button.btn(style="margin-right: 10px") Hover
+    tooltip(anchor="w") Content
+  button.btn(style="margin-right: 10px") Hover
+    tooltip(anchor="e") Content
+
+</template>
+
+<script lang="coffee">
+require("./materialize.config.scss")
+module.exports =
+  components:
+    "tooltip" : require "../src/tooltip.coffee"
+
+</script>
