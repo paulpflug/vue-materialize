0caed202d627bf6b0af50aef6be3d7cfb7287b75
Paul Pflugradt <paulpflugradt@googlemail.com>
2016-08-11 15:50:11 +0200

    Updates

---
+++ babel-import.vue
@@ -0,0 +1,20 @@
+<template lang="jade">
+parallax(src="http://materializecss.com/images/parallax1.jpg")
+  .loading(slot="loading") loading...
+.container(style="height:500px")
+  p
+    a(href="https://github.com/paulpflug/vue-materialize/blob/master/dev/parallax.vue") source
+parallax(src="http://materializecss.com/images/parallax2.jpg")
+  .loading(slot="loading") loading...
+.container(style="height:500px")
+.container(style="height:500px")
+</template>
+
+<script>
+import {parallax} from "../index"
+export default {
+  components: {
+    "parallax": parallax
+  }
+}
+</script>
---
+++ forms/input-field.vue
@@ -0,0 +1,39 @@
+<template lang="jade">
+.container
+  .row
+    input-field.s6(label="name")
+      icon.prefix(name="fa-user",slot="icon")
+    input-field.s6(label="password",type="password")
+      icon.prefix(name="fa-key",slot="icon")
+  .row
+    input-field.s6(label="placeholder",placeholder="placeholder")
+    input-field.s6(label="disabled",disabled)
+  .row
+    input-field.s6(label="readonly",readonly,value="readonly")
+    input-field.s6(label="autofocus",autofocus)
+  .row
+    input-field.s6(label="valid",value="valid",:validate="isValid",data-success="valid")
+    input-field.s6(label="invalid",value="invalid",:validate="isValid",data-error="invalid")
+  .row
+    input-field.s12(label="character-counter",:length=10)
+
+  input-field.s12(label="textarea with autosize",textarea)
+  input-field.s12(label="placeholder",textarea,placeholder="placeholder")
+  input-field.s12(label="prefilled",textarea,value="prefilled")
+  input-field.s12(label="character-counter textarea",:length=120,textarea)
+  p
+    a(href="https://github.com/paulpflug/vue-materialize/blob/master/dev/forms/input-field.vue") source
+</template>
+
+<script lang="coffee">
+module.exports =
+  components:
+    "icon" : require "../../src/icon.coffee"
+    "input-field": require "../../src/input-field.vue"
+  methods:
+    isValid: (value) ->
+      return true if value == "valid"
+      return false if value == "invalid"
+      return null
+
+</script>
